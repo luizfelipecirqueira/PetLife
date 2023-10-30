dba5099a17c460ce72002f07fd1e700a95dcf870
luizfcs <luiz.cirqueira@sergipetec.org.br>
2023-10-30 10:59:52 -0300

    Ajustes no css e implementação de efeito com javascript para finalização da landpage

---
+++ index.js
@@ -0,0 +1,7 @@
+var elementos = document.querySelectorAll('.duvida');
+
+elementos.forEach(function (duvida){
+    duvida.addEventListener('click', function(){
+        duvida.classList.toggle("ativa");
+    })
+});
